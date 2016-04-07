bf1d9ca952ca145b2cd993632a12b778e4efeede
Tyler Arbon <tylercd100@gmail.com>
2016-04-06 19:57:25 -0600

    Fixed Twilio. Added more unit tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to `laravel-sms` will be documented in this file.
 
+### 1.2.1
+- Fixed Twilio
+- Added more unit tests
+
 ### 1.2.0
 - Added Twilio support
 
--- tests/SmsTest.php
+++ tests/SmsTest.php
@@ -23,9 +23,48 @@ class SmsTest extends TestCase
         }
     }
 
+    public function testItThrowsExceptionWhenUsingInvalidPhoneNumberArray(){
+        $sms = new Sms();
+        $this->setExpectedException(InvalidPhoneNumberException::class);
+        $result = $sms->validatePhoneNumbers(["(734) 555 1212","asdf"],true);
+    }
+
     public function testItThrowsExceptionWhenUsingInvalidPhoneNumber(){
         $sms = new Sms();
         $this->setExpectedException(InvalidPhoneNumberException::class);
-        $result = $sms->validatePhoneNumbers(["5552017374","asdf"],true);
+        $result = $sms->validatePhoneNumber("asdf",true);
+    }
+
+    public function testItPassesAValidPhoneNumber(){
+        $sms = new Sms();
+        $result = $sms->validatePhoneNumber("(734) 555 1212",true);
+    }
+
+    public function testSmsSend(){
+        $sms = $this->getSmsInstanceWithMockedDriver();
+        $sms->send("message","(734) 555 1211","(734) 555 1212");
+    }
+
+    public function testSmsSendMany(){
+        $sms = $this->getSmsInstanceWithMockedDriver();
+        $sms->sendMany("message",["(734) 555 1213","(734) 555 1214"],"(734) 555 1215");
+    }
+
+    public function testSmsSendArray(){
+        $sms = $this->getSmsInstanceWithMockedDriver();
+        $sms->sendArray([
+            ["msg"=>"message","to"=>"(734) 555 1214","from"=>"(734) 555 1215"],
+            ["msg"=>"message","to"=>"(734) 555 1213","from"=>"(734) 555 1216"],
+            ["msg"=>"message","to"=>"(734) 555 1212","from"=>"(734) 555 1217"],
+        ]);
+    }
+
+    protected function getSmsInstanceWithMockedDriver(){
+        $mock = $this->getMock('Zenapply\Sms\Drivers\Plivo\Request', ['send'], ['user','token']);
+        
+        $mock->expects($this->any())
+             ->method('send');
+
+        return new Sms($mock);
     }
 }
