f9b2cf799c2262f39ad241a3d3b755ffccdf70a1
Tyler Arbon <tylercd100@gmail.com>
2016-04-07 16:36:30 -0600

    Added functions to buy and sell numbers (#3)
    * Added functions to buy and sell numbers
    
    * fixed twilio sell numbers

--- src/Drivers/Request.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Zenapply\Sms\Drivers;
-
-abstract class Request implements SendsSms
-{
-
-}
---
+++ src/Drivers/Twilio.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Zenapply\Sms\Drivers;
+
+use Services_Twilio as Service;
+use Zenapply\Sms\Drivers\Driver;
+
+class Twilio extends Driver {
+
+    protected $handle;
+
+    protected $auth_id;
+
+    public function __construct($auth_id, $auth_token){
+        $this->auth_id = $auth_id;
+        $this->handle = new Service($auth_id, $auth_token);
+    }
+
+    public function send($msg,$to,$from){
+        return $this->handle->account->messages->sendMessage($from, $to, $msg);
+    }
+
+    public function searchNumber($areacode,$country = 'US'){
+        $resp = $this->handle->account->available_phone_numbers->getList($country, 'Local', [
+            "AreaCode" => $areacode,
+            "Sms" => true,
+        ]);
+
+        return $this->respond($resp);
+    }
+
+    public function buyNumber($phone){
+        $resp = $this->handle->account->incoming_phone_numbers->create(array(
+            "PhoneNumber" => $phone,
+        ));
+        return $this->respond($resp);
+    }
+
+    public function sellNumber($phone){
+        $sid = false;
+        foreach ($this->handle->account->incoming_phone_numbers as $number) {
+            if($phone === $number->phone_number){
+                $sid = $number->sid;
+            }
+        }
+        $resp = $this->handle->account->incoming_phone_numbers->delete($sid);
+        return $this->respond($resp);
+    }
+
+    private function respond($data){
+        return json_decode(json_encode($data), true);
+    }
+}
